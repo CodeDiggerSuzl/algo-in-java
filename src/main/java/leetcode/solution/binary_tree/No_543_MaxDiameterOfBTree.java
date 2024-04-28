388f881b8a71c57d374e0512671933f7d7f4aa9f
sup3r z1_ <joexsue@foxmail.com>
2024-04-28 23:21:46 +0800

    [!]树的直径

--- src/main/java/leetcode/solution/binary_tree/No_543_MaxDiameterOfBTree.java
+++ src/main/java/leetcode/solution/binary_tree/No_543_MaxDiameterOfBTree.java
@@ -2,64 +2,72 @@ package leetcode.solution.binary_tree;
 
 import annotion.LongTime;
 import annotion.Stocked;
+import annotion.ToDo;
 import leetcode.solution.binary_tree.common.BTreeUtil;
 import leetcode.solution.binary_tree.common.TreeNode;
 import org.junit.Test;
 
-/*
- *
+/**
  * [name]:
  * [link]:
  * [description]:
  * 给你一棵二叉树的根节点，返回该树的直径 。
  * 二叉树的 直径 是指树中任意两个节点之间最长路径的 长度 。这条路径可能经过也可能不经过根节点 root 。
  * 两节点之间路径的 长度 由它们之间边数表示。
- *
- * [category]:
- * [solving tips]:
- *
+ * 1
+ * / \
+ * /   \
+ * 2   3
+ * / \
+ * 4 5
+ * [category]: 二叉树
+ * [solving tips]: 无法仅仅通过递归去做, 是通过递归和遍历结合的方法
  */
 @Stocked
 @LongTime
+@ToDo(doWhat = "重新梳理思路")
 public class No_543_MaxDiameterOfBTree {
 
 
-    private int max = 0;
-
+    private int ans = 0;
 
     public int diameterOfBinaryTree(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        if (root.left == null && root.right == null) {
-            return 1;
-        }
-        int left = diameterOfBinaryTree(root.left);
-        int right = diameterOfBinaryTree(root.right);
-        max = Math.max(left + right, max);
-        return max;
+        dfs_2(root);
+        return ans;
     }
 
+    /**
+     * https://www.bilibili.com/video/BV17o4y187h1/?spm_id_from=333.880.my_history.page.click&vd_source=aca956c6b30e207641786b78016d94ff
+     */
+    public int dfs(TreeNode root) {
+        if (root == null) {
+            return -1;
+        }
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        ans = Math.max(left + right + 2, ans);
+        return Math.max(left, right) + 1;
+    }
 
-    public int diameterOfBinaryTree2(TreeNode root) {
+    /**
+     * 这个方法似乎更好理解一点
+     * https://www.bilibili.com/video/BV1CK4y1Y76h/?spm_id_from=333.880.my_history.page.click&vd_source=aca956c6b30e207641786b78016d94ff
+     */
+    public int dfs_2(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        if (root.left == null && root.right == null) {
-            return 0;
-        }
-        int left = diameterOfBinaryTree2(root.left);
-        int right = diameterOfBinaryTree2(root.right);
-        max = Math.max(left + right + 2, max) - 1;
-        return max;
+        int left = dfs_2(root.left);
+        int right = dfs_2(root.right);
+        ans = Math.max(left + right, ans);
+        return Math.max(left, right) + 1;
     }
 
 
     @Test
     public void test() {
         TreeNode node = BTreeUtil.createTree("[1,2,3,4,5]");
-        int i = diameterOfBinaryTree2(node);
-        System.out.println("diameterOfBinaryTree(node) = " + (i));
+        System.out.println("diameterOfBinaryTree(node) = " + diameterOfBinaryTree(node));
     }
 
 }
