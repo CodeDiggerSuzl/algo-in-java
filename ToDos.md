1b8368cbddf928ae41a1bcd27ca698b8e3f8c550
sup3r z1_ <joexsue@foxmail.com>
2024-05-16 22:43:58 +0800

    [x]:654

--- ToDos.md
+++ ToDos.md
@@ -1,23 +1,32 @@
 ### 2024.04.25
-- [ ] dfs & bfs 
+
+- [ ] dfs & bfs
 - [ ] Java 中的 queue 和 stack API
 
 看了 No.2385 的题解后的答案:
 需要做以下几题:
+
 - [x] [104 二叉树的最大深度](https://leetcode.cn/problems/maximum-depth-of-binary-tree/)
 - [x] [543 二叉树的直径](https://leetcode.cn/problems/diameter-of-binary-tree/description/)
 - [x] [2385 感染二叉树需要的总时间](https://leetcode.cn/problems/amount-of-time-for-binary-tree-to-be-infected/description/?envType=daily-question&envId=2024-04-24)
-- [x] 然后再做 [863. 二叉树中所有距离为 K 的结点 - 力扣（LeetCode）](https://leetcode.cn/problems/all-nodes-distance-k-in-binary-tree/description/)
-
-
+- [x] 
+  然后再做 [863. 二叉树中所有距离为 K 的结点 - 力扣（LeetCode）](https://leetcode.cn/problems/all-nodes-distance-k-in-binary-tree/description/)
 
 ### 2024.04.29
+
 单个二叉树相关的问题:
+
 1. 二叉树的遍历
 2. 二叉的深度
-   3. 最大深度
-   4. 最小深度
+    3. 最大深度
+    4. 最小深度
 5. 二叉树的直径(二叉树中距离最长的 2 个点之间的距离)
-   6. 经过特定点的最大距离(No.2385)
+    6. 经过特定点的最大距离(No.2385)
 6. 距离二叉树根节点特定距离的节点
 7. 距离二叉树中某个节点特定距离的节点(No.863)
+
+### 2024.05.16
+
+- [654. 最大二叉树使用单调栈解法 - 力扣（LeetCode）](https://leetcode.cn/problems/maximum-binary-tree/solutions/1762400/zhua-wa-mou-si-by-muse-77-myd7/)
+- [114. 二叉树展开为链表4种解法 - 力扣（LeetCode）](https://leetcode.cn/problems/flatten-binary-tree-to-linked-list/solutions/131121/dong-hua-yan-shi-si-chong-jie-fa-114-er-cha-shu-zh)]
+- 数组的拷贝方法
---
+++ src/main/java/leetcode/solution/binary_tree/No_654_M_MaximumBTree.java
@@ -0,0 +1,137 @@
+package leetcode.solution.binary_tree;
+
+import annotion.ToDo;
+import leetcode.solution.binary_tree.common.BTreePrinter;
+import leetcode.solution.binary_tree.common.TreeNode;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+/**
+ * [name]: 654. 最大二叉树
+ * <p>
+ * [link]: https://leetcode.cn/problems/maximum-binary-tree/description/
+ * <p>
+ * [description]:
+ * <p>
+ * [category]:
+ * <p>
+ * [solving tips]:
+ * <p>
+ * <p>
+ * [answers]:
+ */
+@Slf4j
+@ToDo(value = "单调栈解法")
+// https://leetcode.cn/problems/maximum-binary-tree/solutions/1762400/zhua-wa-mou-si-by-muse-77-myd7/
+public class No_654_M_MaximumBTree {
+
+
+    /**
+     * 第三种方法不用 split
+     */
+    public TreeNode constructMaximumBinaryTree_1(int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
+        int maxIdx = getMaxIdx(nums);
+        log.debug("maxIdx={},nums={}", maxIdx, nums.length);
+        TreeNode left = constructMaximumBinaryTree_1(0 <= maxIdx ? Arrays.copyOfRange(nums, 0, maxIdx) : new int[]{});
+        TreeNode right = constructMaximumBinaryTree_1(maxIdx + 1 <= nums.length ? Arrays.copyOfRange(nums, maxIdx + 1, nums.length) : new int[]{});
+        return new TreeNode(nums[maxIdx], left, right);
+    }
+
+    public int getMaxIdx(int[] nums) {
+        if (nums.length == 0) {
+            return -1;
+        }
+
+        int maxIdx = -1;
+        int maxVal = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > maxVal) {
+                maxIdx = i;
+                maxVal = nums[i];
+            }
+        }
+        return maxIdx;
+    }
+
+    @Test
+    public void test_1() {
+        int[] arr = {3, 2, 1, 6, 0, 5};
+        TreeNode treeNode = constructMaximumBinaryTree_1(arr);
+        BTreePrinter.printTree(treeNode);
+    }
+
+
+    /* -----------------------------------------------  ----------------------------------------------- */
+
+    @Test
+    public void test_arrSplit() {
+        int[] arr = {3, 2, 1, 6, 0, 5};
+        int[] ints = Arrays.copyOfRange(arr, 0, 6);
+        // 该方法前包后不包
+        log.debug("arr split={}", ints);
+    }
+
+    public TreeNode constructMaximumBinaryTree_2(int[] nums) {
+        Deque<TreeNode> deque = new ArrayDeque();
+        for (int i = 0; i < nums.length; i++) {
+            TreeNode node = new TreeNode(nums[i]);
+            while (!deque.isEmpty()) {
+                TreeNode topNode = deque.peekLast();
+                if (topNode.val > node.val) {
+                    deque.addLast(node);
+                    topNode.right = node;
+                    break;
+                } else {
+                    deque.removeLast(); // 出栈操作
+                    node.left = topNode;
+                }
+            }
+            if (deque.isEmpty()) deque.addLast(node);
+        }
+        return deque.peek();
+    }
+
+    //    作者：爪哇缪斯
+    //    链接：https://leetcode.cn/problems/maximum-binary-tree/solutions/1762400/zhua-wa-mou-si-by-muse-77-myd7/
+    //    来源：力扣（LeetCode）
+    //    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    //
+
+    /* ----------------------------------------------- method3 ----------------------------------------------- */
+    /* 主函数 */
+    TreeNode constructMaximumBinaryTree_3(int[] nums) {
+        return build(nums, 0, nums.length - 1);
+    }
+
+    // 定义：将 nums[lo..hi] 构造成符合条件的树，返回根节点
+    TreeNode build(int[] nums, int lo, int hi) {
+        // base case
+        if (lo > hi) {
+            return null;
+        }
+
+        // 找到数组中的最大值和对应的索引
+        int index = -1, maxVal = Integer.MIN_VALUE;
+        for (int i = lo; i <= hi; i++) {
+            if (maxVal < nums[i]) {
+                index = i;
+                maxVal = nums[i];
+            }
+        }
+
+        // 先构造出根节点
+        TreeNode root = new TreeNode(maxVal);
+        // 递归调用构造左右子树
+        root.left = build(nums, lo, index - 1);
+        root.right = build(nums, index + 1, hi);
+
+        return root;
+    }
+}
