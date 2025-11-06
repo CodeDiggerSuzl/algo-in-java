9982211eea66d1b24fe19fe0ecbc5824636ec3dc
suzailong <suzailong@didachuxing.com>
2025-11-06 18:56:35 +0800

    4sum

--- src/main/java/leetcode/solution/tow_pointer/Solution.java
+++ src/main/java/leetcode/solution/tow_pointer/Solution.java
@@ -120,15 +120,69 @@ public class Solution {
     }
 
     /* -----------------------------------------------  ----------------------------------------------- */
-
+    // 给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[b], nums[c], nums[d]] （若两个四元组元素一一对应，则认为两个四元组重复）：
+    //
+    //0 <= a, b, c, d < n
+    //a、b、c 和 d 互不相同
+    //nums[a] + nums[b] + nums[c] + nums[d] == target
+    //你可以按 任意顺序 返回答案 。
+    //
+    //示例 1：
+    //
+    //输入：nums = [1,0,-1,0,-2,2], target = 0
+    //输出：[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+    //示例 2：
+    //
+    //输入：nums = [2,2,2,2,2], target = 8
+    //输出：[[2,2,2,2]]
     // [18. 四数之和 - 力扣（LeetCode）](https://leetcode.cn/problems/4sum/description/)
     public List<List<Integer>> fourSum(int[] nums, int target) {
-
+        Arrays.sort(nums);
+        int len = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < len - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < len - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int left = j + 1, right = len - 1;
+                while (left < right) {
+                    long sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[left]);
+                        list.add(nums[right]);
+                        ans.add(list);
+                        left++;
+                        right--;
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    } else if (sum > target) {
+                        right--;
+                    } else {
+                        left++;
+                    }
+                }
+            }
+        }
+        return ans;
     }
 
     @Test
     public void test_18() {
-
+        int[] arr = {2, 2, 3, 1, 2, 2};
+        List<List<Integer>> lists = fourSum(arr, 8);
+        log.info("ans={}", JsonUtil.toJson(lists));
     }
     /* -----------------------------------------------  ----------------------------------------------- */
+
 }
