b0d17925b94b111e9f858257b9b30c9af79d1bb4
sup3r z1_ <joexsue@foxmail.com>
2025-12-30 21:52:14 +0800

    no1

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -98,4 +98,38 @@ public class Solution {
         System.out.println(substring);
     }
 
+    /**
+     * 5. 最长回文子串
+     * 作者：灵茶山艾府
+     * 链接：https://leetcode.cn/problems/longest-palindromic-substring/solutions/2958179/mo-ban-on-manacher-suan-fa-pythonjavacgo-t6cx/
+     */
+    public String longestPalindrome(String S) {
+        char[] s = S.toCharArray();
+        int n = s.length;
+        int ansLeft = 0;
+        int ansRight = 0;
+        for (int i = 0; i < 2 * n - 1; i++) {
+            int l = i / 2;
+            int r = (i + 1) / 2;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                l--;
+                r++;
+            }
+            // 循环结束后，s[l+1] 到 s[r-1] 是回文串
+            if (r - l - 1 > ansRight - ansLeft) {
+                ansLeft = l + 1;
+                ansRight = r; // 左闭右开区间
+            }
+        }
+        return S.substring(ansLeft, ansRight);
+    }
+
+
+    @Test
+    public void test_5() {
+        String s = "acbc";
+        String s1 = longestPalindrome(s);
+        System.out.println("s1 = " + s1);
+    }
+
 }
